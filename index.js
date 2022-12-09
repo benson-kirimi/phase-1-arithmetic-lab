ad9c8bb0fcca1e6e0a4c027dff9b6570e9b3f6bc
benson-kirimi <bensongatobu2019@mail.com>
2022-12-09 15:59:20 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+function mutiply(31,2){
+    return 31*2;
+}
+console.log(multiply(32,2));
+// 62
+function randomNumber(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  function mod 
+  var remainder;
+  remainder = 19 % 3;
+function max 
+let counter = 20;
+//=> defined
+
+++counter;
+//=> 20
+
+counter;
+//=> 20
+
+
